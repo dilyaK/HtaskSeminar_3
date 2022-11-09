26795647d7b8d40dbc98ce25e95a0a3bbf5d3da3
dilya <1dilnora777@gmail.com>
2022-11-10 01:02:22 +0500

    README added

---
+++ task003/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A(3,6,8); B(2,1,-7)->15.84
+
+float x1, y1, z1;
+Console.Write("Enter the x coordinates of the first point: ");
+x1 = Convert.ToSingle(Console.ReadLine()); 
+Console.Write("Enter the y coordinates of the first point: ");
+y1 = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter the z coordinates of the first point: ");
+z1 = Convert.ToSingle(Console.ReadLine());
+
+float x2, y2, z2;
+
+
