1f375a107d6ee273ac38072fc9f7a7af02a52134
dilya <1dilnora777@gmail.com>
2022-11-10 00:20:53 +0500

    gitignore_added

---
+++ task001/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет является ли оно палиндромом.
+// 14212->нет,  12821->да,  23432->да.
+
+Console.WriteLine("Enter five-digit number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int digit1 = number / 10000;
+int digit2 = number / 1000 % 10;
+int digit3 = number / 100 % 10;
+int digit4 = number / 10 % 10;
+int digit5 = number % 10;
+
+if(digit1 == digit5 && digit2 == digit4)
+{
+   Console.WriteLine("Yes");
+}
+else
+{
+   Console.WriteLine("No");
+}
